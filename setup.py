20d034912e504a99826883a07c8b049fd530b95a
Keerthan Jaic <keerthan@swiftnav.com>
2017-08-03 00:38:28 -0700

    use setuptools_scm

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="snavutils",
-    version="0.1.0",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     install_requires=['numpy'],
     extras_require={
         'test': ['pytest'],
