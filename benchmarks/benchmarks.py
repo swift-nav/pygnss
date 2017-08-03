d63a6839c2976d3950368efb048b781ec70a9e84
Keerthan Jaic <keerthan@swiftnav.com>
2017-08-03 03:29:29 -0700

    add asv config and sample benchmark

---
+++ benchmarks/benchmarks.py
@@ -0,0 +1,13 @@
+import snavutils.coord_system as cs
+
+class TimeSuite:
+    """
+    An example benchmark that times the performance of various kinds
+    of iterating over dictionaries in Python.
+    """
+    def setup(self):
+        self.a = (38., 122., 0.)
+
+    def time_to_ecef(self):
+        cs.wgsllh2ecef(self.a)
+
