05474f2cedb26b7a08e5a9c4664be9c46a4527eb
Keerthan Jaic <keerthan@swiftnav.com>
2017-09-25 15:11:01 -0700

    update setup.py to add swift contact info

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gnss",
+    name='gnss',
+    description='GNSS Utilities',
+    author='Swift Navigation',
+    author_email='dev@swiftnav.com',
+    url='https://github.com/swift-nav/python-gnss',
+    packages=find_packages(),
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     install_requires=['numpy'],
@@ -11,5 +16,12 @@ setup(
             'hypothesis',
         ],
     },
-    packages=find_packages()
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
