ca2f5813de48608e8bfecbc332bc1a649abc6330
Keerthan Jaic <keerthan@swiftnav.com>
2017-09-26 13:34:33 -0700

    add copyright header to coord_systems.py

--- gnss/coord_system.py
+++ gnss/coord_system.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2017 Swift Navigation Inc.
+# Contact: Swift Navigation <dev@swiftnav.com> 
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+
+
 import numpy as np
 
 WGS84_A = 6378137.0
